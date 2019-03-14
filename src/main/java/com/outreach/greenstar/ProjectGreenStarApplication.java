b02dc59536a6e5243412c3dccba8b35cbfe3df98
nandishkb <encodefox@gmail.com>
2019-03-14 13:04:09 +0530

    added cors configuration

--- src/main/java/com/outreach/greenstar/ProjectGreenStarApplication.java
+++ src/main/java/com/outreach/greenstar/ProjectGreenStarApplication.java
@@ -2,6 +2,10 @@ package com.outreach.greenstar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class ProjectGreenStarApplication {
@@ -9,6 +13,17 @@ public class ProjectGreenStarApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectGreenStarApplication.class, args);
 	}
+	
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+	    
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/v1/**").allowedOrigins("*");
+            }
+        };
+    }
 
 }
 
